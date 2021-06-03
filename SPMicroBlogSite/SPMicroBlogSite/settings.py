a7379c28e71073343c3a23f685edb6d7e61d6a55
Alexjs95 <aj.scotson@hotmail.com>
2021-06-03 11:01:58 +0100

    Moved key to env file which is ignored by git.

--- SPMicroBlogSite/SPMicroBlogSite/settings.py
+++ SPMicroBlogSite/SPMicroBlogSite/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0#e#l$+%f3$==v^74s5c)b2et(x09se(*ceif(i)m*h9esf)-r'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
