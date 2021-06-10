03cd3230ff7ed1592b1f0828cbb3035352152e40
Alexjs95 <aj.scotson@hotmail.com>
2021-06-10 12:08:50 +0100

    implemented snowplow python tracker to track specific events within the blog application, updated readme too.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Creation of a simple static blog site using Django. The site will later be used to apply Snowplow tracking libraries.
+
+Useage:
+in SPMicroBlog execute  'python manage.py makemigrations'
+			'python manage.py makemigrations'
+			'python manage.py runserver'
+
+Navigate to localhost:8000/admin
+Login with superuser, create with command 'python manage.py createsuperuser'
+Should see two models under BLOG - Categorys and posts
+Create a couple of categories then create some fake posts and assign category.
+
+Navigate to localhost:8000/blog
+Posts should be displayed if created.
+
+Snowplow tracking implemented in \SPMicroBlogSite\blog\views.py
--- SPMicroBlogSite/blog/views.py
+++ SPMicroBlogSite/blog/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
 from .forms import CommentForm
+from snowplow_tracker import Tracker, Emitter, Subject
 
-
+e = Emitter("d3rkrsqld9gmqf.cloudfront.net")
+s = Subject().set_user_id("AJS").set_lang("eng")    # track main user
+t = Tracker(e)
+    
 def index_blog(request):
     posts = Post.objects.all().order_by("-created_on")
     context = {"posts": posts}
+    
+    t.track_page_view("localhost:8000/blog", "index")   # page view track
+
     return render(request, "index_blog.html", context)
 
 
@@ -14,23 +21,28 @@ def blog_category(request, category):
         "-created_on"
     )
     context = {"category": category, "posts": posts}
+    
+    t.track_page_view("localhost:8000/blog/$category", "blog category") # page view track
+    
     return render(request, "blog_category.html", context)
 
 
 def blog_post(request, pk):
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
-
+    t.track_page_view("localhost:8000/blog/$id", "blog post view") # page view track
+    
     form = CommentForm()
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = Comment(
-                author=form.cleaned_data["author"],
-                body=form.cleaned_data["body"],
-                post=post,
+                author = form.cleaned_data["author"],
+                body = form.cleaned_data["body"],
+                post = post,
             )
             comment.save()
+            t.track_form_submit("CommentForm")      # Track that commentform been submitted
 
     context = {"post": post, "comments": comments, "form": form}
     return render(request, "blog_post.html", context)
